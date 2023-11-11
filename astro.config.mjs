b267fcbd83c943216d86749ab4711e01747f6703
Juan Martin <martinmuda@gmail.com>
2023-11-10 21:06:07 -0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zumerlab.github.io/docs',
+  site: 'https://zumerlab.github.io',
+  base: '/docs',
   integrations: [
     starlight({
       title: 'Orbit',
